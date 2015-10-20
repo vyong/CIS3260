00548b84a0f31feeb5e14679d82e64fafa0fa5c6
Vincent Yong <yong_vince850@yahoo.ca>
2015-10-20 17:15:20 -0400

    layout of each method inserted

--- Rules.java
+++ Rules.java
@@ -34,24 +34,55 @@ public class Rules{
 	public boolean validAttack(Piece p1, Piece p2, int x1, int y1, int x2, int y2){
 		//Checks whether the attack is valid, returns true or false.
 		
+		/*
+		Check if piece 1 is current player's piece, return false if not
+		check to make sure cell 1 has a piece of current player
+
+		Check if piece 2 is opponent's piece, return false if not
+		Check if cell 2 has opponent piece
+
+		check if cell 1 is beside cell 2
+
+		check third cell to make sure it's empty
+		*/
 		return false;
 	}
 
 	public boolean vaildMove(Piece p1, int x1, int y1, int x2, int y2){
 		//Checks whether the move is valid, return true or false.
 		
+		/*
+		check if piece 1 is current player's piece, return false if not
+		check to make sure cell 1 is piece of current player
+
+		check is cell 2 is beside cell 1, return false if not
+		*/
 		return false;
 	}
 	
 	public boolean validPlace(Piece p1, int x, int y){
 		//Checks whether the placement is valid, returns true or false.
 		
+		/*
+		check if player has pieces and right color, return false if no more pieces
+		check if cell is empty, return false if cell isn't empty
+		
+		if empty cell, placement is valid, return true
+
+		if cell isn't empty return false
+		*/
 		return false;
 	}
 	
 	public boolean validTake(Piece p1, int x, int y){
 		//Checks whether the removal is valid, returns true or false
 		
+		/*
+		check if piece is correct color of opponents, return false if not enemy color
+
+		check if piece in cell contains opponent's piece, return false if not
+
+		*/
 		return false;
 	}
 }
