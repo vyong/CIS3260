e88a0f12a6605c1fe127ba31a8e72e2a4fb8520b
Vincent Yong <yong_vince850@yahoo.ca>
2015-10-21 15:08:45 -0400

    updated rules and updated player class

--- Player.java
+++ Player.java
@@ -1,9 +1,9 @@
 /************************* Player.java ****************************
 Student Name: 						Student Number: 
-Devin Dagg
+Devin Dagg							0852134
 Erik Zorn - Wallentin 				0864583
-Taha Ansari
-Vincent Yong
+Taha Ansari							0849535
+Vincent Yong						0744993
  
 Date: Thur, Oct.15 / 2015			Course Name: CIS3260
 I have exclusive control over this submission via my password.
@@ -25,11 +25,13 @@ public class Player{
 		WHITE, BLACK
 	}
 	
+	private int pieceCount;
 	private Colour colour;
 	
 	private boolean turn = false;
 	
 	public Player(){
+		pieceCount = 0;
 		colour = Colour.WHITE;
 		turn = false;
 	}
@@ -52,13 +54,30 @@ public class Player{
 	public boolean placePiece(Board gameBoard, Piece toPlace, int x, int y){
 		//Using gameBoard call checkPlacment to see if the placement is valid, if return is true, 
 		//sets the piece to the cell. Returns true or false.
-		
-		return false;
+		boolean result = false;
+
+		result = gameBoard.checkPlacment(x,y);
+		if(result == true){
+			//
+			//UPDATE THE CELL in gameBoard
+			//
+		}
+		return result;
 	}
 	
 	public boolean movePiece(Board gameBoard, int x1, int y1, int x2, int y2){
 		//Using gameBoard call checkMove to see if the move is valid, if return is true, 
 		//sets the piece to the cell. Returns true or false.
+		boolean result = false;
+
+		result = gameBoard.checkMove(x1,y1,x2,y2);
+
+		if(result == true){
+			//
+			//UPDATE THE CELL in gameBoard
+			//
+		}
+		return result;
 		
 		return false;
 	}
@@ -66,20 +85,58 @@ public class Player{
 	public boolean attackPiece(Board gameBoard, int x1, int y1, int x2, int y2){
 		//Using gameBoard call checkAttack to see if the attack is valid, if return is true, 
 		//sets the piece to the cell. Calls takePiece to take remove pieces from the board. Returns true or false.
-		
-		return false;
+		boolean result = false;
+
+		result = gameBoard.checkAttack(x1,y1,x2,y2);
+
+		if(result == true){
+			//
+			//UPDATE THE CELL in gameBoard
+			//
+
+			//find the cell that is being taken.
+			if(x1 == x2){
+				if(y1 > y2){
+					y1 = y1-1;
+				} else{
+					y1 = y1+1;
+				}
+			} else{
+				if(x1 > x2){
+					x1 = x1-1;
+				} else{
+					x1 = x1+1;
+				}
+
+			}
+			//remove the piece from the cell
+			takePiece(gameBoard,x1,y1);
+		}
+		return result;
 	}
 	
 	public boolean takePiece(Board gameBoard, int x, int y){
 		//Allows the player to choose what pieces they would like to remove and 
 		//removes any pieces that have been attacked. Returns true or false
-		
+		Piece value = null;
+
+		value = gameBoard.checkCell(x,y);
+		if(value != null){
+			//
+			//UPDATE THE CELL in gameBoard
+			//
+			return true;
+		}
 		return false;
 	}
 	
 	
 	
 	//Getter's
+
+	public int getPieceCount(){
+		return pieceCount;
+	}
 	
 	public Colour getColour(){
 		return colour;
@@ -91,6 +148,10 @@ public class Player{
 
 	//Setter's
 	
+	public void setPieceCount(int count){
+		pieceCount = count;
+	}
+
 	public void setColour(Colour pColour){
 		colour = pColour;
 	}
--- Rules.java
+++ Rules.java
@@ -33,27 +33,184 @@ public class Rules{
 	
 	public boolean validAttack(Piece p1, Piece p2, int x1, int y1, int x2, int y2){
 		//Checks whether the attack is valid, returns true or false.
-		
-		
-		
-		return false;
+
+
+		/* DEV NOTES
+		Assumptions of parameters:
+		p1: current player's piece
+		p2: opponent's piece
+		x1, y1: current player's piece coordinate
+		x2, y2: opponent's piece on the board
+		*/
+
+		//Check if piece 1 is current player's piece, return false if not
+		Player playerColour = getColour();
+		if (p1.getColour() != playerColour) {
+			return false;
+		}
+
+		//Check if piece 2 is opponent's piece, return false if not
+		if (p2.getColour() == playerColour) {
+			return false;
+		}
+
+		//check to make sure cell 1 has a piece of current player
+		Piece cellPiece = checkCell(x1, y1);
+		if(cellpiece.colour != playerColour || cellPiece == NULL) {
+			return false;
+		}
+
+		//Check if cell 2 has opponent piece
+		cellPiece = checkCell(x2, y2);
+		if(cellpiece.colour == playerColour || cellPiece == NULL) {
+			return false;
+		}
+
+		int xDiff = x2 - x1;
+		int yDiff = y2 - y1;
+
+		//check if cell 1 is beside cell 2
+		if(xDiff == 1 && yDiff == 0){
+			Piece thirdCell = checkCell(x2 + 1, y2);
+
+			if (thirdCell == NULL) {
+				return true;
+			}
+
+			else {
+				return false;
+			}
+		}
+
+		else if (xdiff == -1 && yDiff == 0){
+			Piece thirdCell = checkCell(x2 - 1, y2);
+
+			if (thirdCell == NULL) {
+				return true;
+			}
+
+			else {
+				return false;
+			}
+		}
+		else if( xDiff == 0 && yDiff == 1){
+			Piece thirdCell = checkCell(x2, y2 + 1);
+
+			if (thirdCell == NULL) {
+				return true;
+			}
+
+			else {
+				return false;
+			}
+		}
+		else if( xDiff == 0 && yDiff == -1){
+			Piece thirdCell = checkCell(x2, y2 - 1);
+
+			if (thirdCell == NULL) {
+				return true;
+			}
+
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+
 	}
 
 	public boolean vaildMove(Piece p1, int x1, int y1, int x2, int y2){
 		//Checks whether the move is valid, return true or false.
+
+		/* DEV NOTES
+		Assumption of variables:
+		p1: piece of current player
+		x1, y1:current location of piece
+		x2, y2: destination of current player's piece
+		*/
 		
-		return false;
+		//Check if piece 1 is current player's piece, return false if not
+		Player playerColour = getColour();
+		if (p1.getColour() != playerColour) {
+			return false;
+		}
+
+		//check to make sure cell 1 has a piece of current player
+		Piece cellPiece = checkCell(x1, y1);
+		if(cellpiece.colour != playerColour || cellPiece == NULL) {
+			return false;
+		}
+
+		int xDiff = x2 - x1;
+		int yDiff = y2 - y1;
+		//check if cell 1 is beside cell 2
+		if(xDiff == 1 && yDiff == 0){
+			return true;
+		}
+
+		else if (xdiff == -1 && yDiff == 0){
+			return true;
+		}
+		else if( xDiff == 0 && yDiff == 1){
+			return true;
+		}
+		else if( xDiff == 0 && yDiff == -1){
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
-	public boolean validPlace(Piece p1, int x, int y){
+	public boolean validPlace(Player player1, Piece p1, int x, int y){
 		//Checks whether the placement is valid, returns true or false.
+
+		/* DEV NOTES
+		Assumption of variables:
+		player1: current player
+		p1: piece of current player
+		x1, y1:location of where current player wants to place
+		*/
+
+		if(player1.pieceCount > 12) {
+			return false;
+		}
 		
-		return false;
+		//Check if piece 1 is current player's piece, return false if not
+		Player playerColour = getColour();
+		if (p1.getColour() != playerColour) {
+			return false;
+		}
+
+		//check to make sure cell 1 doesn't have a piece
+		Piece cellPiece = checkCell(x1, y1);
+		if(cellPiece == NULL) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public boolean validTake(Piece p1, int x, int y){
 		//Checks whether the removal is valid, returns true or false
+
+		/* DEV NOTES
+		Assumption of variables:
+		p1: piece of current player
+		x1, y1:location of where current player wants to take piece from
+		*/
 		
-		return false;
+
+		//check to make sure cell 1 has a piece of opponent
+		Piece cellPiece = checkCell(x1, y1);
+		if(cellpiece.colour != p1.colour || cellPiece == NULL) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 }
